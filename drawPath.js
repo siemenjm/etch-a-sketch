495b9f91e31637b0656cdce96d3ec4de1f1d9ea6
Jared Siemen <siemenjm@gmail.com>
2020-10-12 17:56:53 -0400

    Make grid items change to random color on mouseover event.

--- drawPath.js
+++ drawPath.js
@@ -1,4 +1,9 @@
 function drawPath(gridItem) {
-    gridItem.style.backgroundColor = "black";
-    gridItem.style.borderColor = "black";
+    let maxRGBValue = 255;
+    let redValue = Math.random() * maxRGBValue;
+    let greenValue = Math.random() * maxRGBValue;
+    let blueValue = Math.random() * maxRGBValue;
+
+    gridItem.style.backgroundColor = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
+    gridItem.style.borderColor = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
 }

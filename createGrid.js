dd5132d76a3adf75765a024a024292fccb54c891
Jared Siemen <siemenjm@gmail.com>
2020-10-12 12:25:34 -0400

    Rename script.js to createGrid.js to allow for other scripts to be executed.

---
+++ createGrid.js
@@ -0,0 +1,14 @@
+function createGrid(rows, columns) {
+    totalItems = rows * columns;
+    for (i = 1; i <= totalItems; i++) {
+        let item = document.createElement("div");
+        item.classList.add("grid-item");
+        item.textContent = i;
+        document.getElementById("main-container").appendChild(item);
+    }
+
+    document.getElementById("main-container").style.gridTemplateColumns = "repeat(" + columns + ", 1fr)";
+
+}
+
+createGrid(8,8);

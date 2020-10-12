cc894cf94fb62e0edd09addc4e2d441261a024d1
Jared Siemen <siemenjm@gmail.com>
2020-10-12 18:59:43 -0400

    Saving and regrouping for the night lol.

--- drawPath.js
+++ drawPath.js
@@ -11,10 +11,25 @@ function drawPath(gridItem, penColor) {
         gridItem.style.backgroundColor = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
         gridItem.style.borderColor = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
     } else if (penColor === "darken") {
-        let currentRGBValue = gridItem.style.backgroundColor;
-        alert(currentRGBValue);
+        let maxRGBValue = 255;
+        let percentDarker = 0.1;
+        let colorValueChange = maxRGBValue * percentDarker;
+        
+        let currentRGBValue = getComputedStyle(gridItem).backgroundColor;
+        let colorValue = currentRGBValue.slice(4, 7);
+        let newColorValue = colorValue - colorValueChange;
+        let newColorValueString = newColorValue.toString();
 
-        //gridItem.style.backgroundColor = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
-        //gridItem.style.borderColor = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
+        let newRGBValue = currentRGBValue
+        let colorIndex = 1;
+        while (colorIndex <= 3) {
+            newRGBValue = newRGBValue.replace(colorValue, newColorValueString);
+        
+            colorIndex++;
+        }
+        console.log(newRGBValue);
+
+        gridItem.style.backgroundColor = newRGBValue;
+        gridItem.style.borderColor = newRGBValue;
     }
 }
